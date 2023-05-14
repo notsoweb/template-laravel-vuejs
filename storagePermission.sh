9f9f91588a88eecfbac04af49eb786bc649dc83d
Moisés de Jesús Cortés Castellanos <ing.moisesdejesuscortesc@notsoweb.com>
2023-05-14 09:37:46 -0600

    IMPROVE: Pedir usuario sistema al actualizar permisos almacenamiento

--- storagePermission.sh
+++ storagePermission.sh
@@ -1,2 +1,7 @@
-chown -R www-data storage/
-chown -R www-data bootstrap/cache/
+#!/bin/bash
+read -p "Usuario del sistema: " myuser
+
+chown -R $myuser:www-data storage/
+chown -R $myuser:www-data bootstrap/cache/
+chmod -R 775 storage/
+chmod -R 775 bootstrap/cache/
