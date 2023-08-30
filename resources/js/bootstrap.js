d716cbecdbb1ad4f7d8296eacf78ec3a92a7d447
Moisés de Jesús Cortés Castellanos <ing.moisesdejesuscortesc@notsoweb.com>
2023-08-30 13:53:22 -0600

    git add .

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -21,8 +21,6 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 import Echo from 'laravel-echo';
 import Pusher from 'pusher-js';
 
-Pusher.logToConsole = true;
-
 if(import.meta.env.VITE_PUSHER_NOTIFICATIONS == 'true') {
     window.Pusher = Pusher;
     window.Echo = new Echo({
