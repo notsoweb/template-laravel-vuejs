444a2a46c9256e579b1a817664c43d16a7d018a1
Moisés de Jesús Cortés Castellanos <ing.moisesdejesuscortesc@notsoweb.com>
2023-12-20 12:59:56 -0600

    FIX: Falta de referencia a computed en componente Textarea

--- resources/js/Components/Dashboard/Form/Textarea.vue
+++ resources/js/Components/Dashboard/Form/Textarea.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { onMounted, ref } from 'vue';
+import { onMounted, ref, computed } from 'vue';
 
 import Error from '@/Components/Dashboard/Form/Elements/Error.vue';
 import Label from '@/Components/Dashboard/Form/Elements/Label.vue';
